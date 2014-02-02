815072136fc46001b69a7efc7e4bf6a82547be6b
Matthias Pfeil <matthias.pfeil@wwu.de>
2014-02-02 23:35:23 +0100

    Added register page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 /*global angular */
 'use strict';
 
-var app = angular.module("app", ["ngRoute","ngResource","leaflet-directive"]);
+var app = angular.module("app", ["ngRoute","ngResource","leaflet-directive", "xeditable"]);
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -26,6 +26,10 @@ app.config(function($routeProvider) {
         	templateUrl: "views/about.html",
         	controller: "AboutCtrl"
       	})
+        .when('/register', {
+          templateUrl: "views/register.html",
+          controller: "RegisterCtrl"
+        })
       	.otherwise({
         	template: "This doesn't exist!"
       	})
@@ -47,6 +51,8 @@ app.controller("AppCtrl", function($scope) {
 
 app.controller("ExploreCtrl", ["$scope","OpenSenseBoxes", function($scope, OpenSenseBoxes) {
   $scope.data = {};
+  $scope.accordion = {};
+  $scope.accordion.active = "";
 
   OpenSenseBoxes.query(function(response) {
     // Assign the response INSIDE the callback
@@ -54,10 +60,26 @@ app.controller("ExploreCtrl", ["$scope","OpenSenseBoxes", function($scope, OpenS
   });
 
   // $scope.productId = $routeParams.state;
-  $scope.templates =
-    [ { name: 'Geräte', url: 'views/devices.html'}
-    , { name: 'Sensoren', url: 'template.html'} ];
-  $scope.template = $scope.templates[0];
+  // $scope.templates =
+  //   [ { name: 'Geräte', url: 'views/devices.html'}
+  //   , { name: 'Sensoren', url: 'template.html'} ];
+  // $scope.template = $scope.templates[0];
+
+  $scope.show = function (boxId){
+    var elem = "#"+boxId;
+
+    if ($scope.accordion.active == elem) {
+      angular.element($scope.accordion.active).removeClass("active");
+      $scope.accordion.active = "";
+    } else {
+      if ($scope.accordion.active != ""){
+        angular.element($scope.accordion.active).removeClass("active");
+      } 
+      
+      angular.element(elem).addClass("active");
+      $scope.accordion.active = elem;
+    };
+  }
 }]);
 
 app.controller("GeoJSONController", [ '$scope', 'OpenSenseBoxes', function($scope, OpenSenseBoxes) {
@@ -105,4 +127,68 @@ app.controller('UploadCtrl', function ($scope) {
 
 app.controller('AboutCtrl', function ($scope) {
 	
+});
+
+app.controller('RegisterCtrl', function($scope, $filter, $http) {
+ $scope.newSenseBox = {};
+
+ $scope.sensors = [
+ ]; 
+
+ angular.extend($scope, {
+        center: {
+            lat: 52,
+            lng: 7,
+            zoom: 10
+        },
+        defaults: {
+            scrollWheelZoom: false
+        },
+        markers: {
+            box: {
+                lat: 52,
+                lng: 7,
+                focus: true,
+                draggable: true
+            },
+        }
+    });
+
+  $scope.phenomenoms = [
+    {value: 1, text: 'Temperature'},
+    {value: 2, text: 'Humidity'},
+    {value: 3, text: 'Wind direction'},
+    {value: 4, text: 'Wind speed'}
+  ]; 
+
+  $scope.showStatus = function(sensor) {
+    var selected = [];
+    if(sensor.phenomenom) {
+      selected = $filter('filter')($scope.phenomenoms, {value: sensor.phenomenom});
+    }
+    return selected.length ? selected[0].text : 'Not set';
+  };
+
+  // remove sensor
+  $scope.removeSensor = function(index) {
+    $scope.sensors.splice(index, 1);
+  };
+
+  //build json object fpr api
+  $scope.saveToDB = function() {
+    $scope.newSenseBox.sensors = $scope.sensors;
+    console.log($scope.markers.box.lat+":"+$scope.markers.box.lng);
+    console.log($scope.newSenseBox);
+  };
+
+  // add sensor
+  $scope.addSensor = function() {
+    $scope.inserted = {
+      id: $scope.sensors.length+1,
+      phenomenom: '',
+      unit: null,
+      type: null 
+    };
+    $scope.sensors.push($scope.inserted);
+  };
 });

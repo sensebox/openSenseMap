12c63d37887649a765c3ea3496fb82c1fd32ce13
Matthias Pfeil <matthias.pfeil@wwu.de>
2014-02-21 20:09:20 +0000

    refactor and added chart.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 /*global angular */
 'use strict';
 
-var app = angular.module("app", ["ngRoute","ngResource","leaflet-directive", "xeditable", "angularCharts"])
+var app = angular.module("app", ["ngRoute","ngResource","leaflet-directive", "xeditable", "angles"])
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -55,78 +55,45 @@ app.controller("AppCtrl", function($scope) {
 
 app.controller("ExploreCtrl", function($scope, OpenSenseBoxes, $http) {
 
+  $scope.chart = {
+    labels : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    datasets : [
+        {
+            fillColor : "rgba(151,187,205,0)",
+            strokeColor : "#e67e22",
+            pointColor : "rgba(151,187,205,0)",
+            pointStrokeColor : "#e67e22",
+            data : [45, 32, 51, 44, 63]
+        },
+        {
+            fillColor : "rgba(151,187,205,0)",
+            strokeColor : "#f1c40f",
+            pointColor : "rgba(151,187,205,0)",
+            pointStrokeColor : "#f1c40f",
+            data : [17.4, 19.8, 20.1, 15.3, 14.5]
+        }
+    ], 
+  };
+
+  $scope.options = {
+    
+  }
+
   $scope.data = {};
   $scope.accordion = {};
   $scope.accordion.active = "";
   $scope.selected = {};
-
-  $scope.data1 = {
-    series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
-    data : [{
-      x : "Sales",
-      y: [100,500, 0],
-      tooltip:"this is tooltip"
-    },
-    {
-      x : "Not Sales",
-      y: [300, 100, 100]
-    },
-    {
-      x : "Tax",
-      y: [351]
-    },
-    {
-      x : "Not Tax",
-      y: [54, 0, 879]
-    }]     
-  }
-
-  $scope.chartType = 'bar';
-
-  $scope.config = {
-    labels: false,
-    title : "Not Products",
-    legend : {
-      display:true,
-      position:'left'
-    }
-  }
-
-  $scope.config1 = {
-    labels: false,
-    title : "Products",
-    legend : {
-      display:true,
-      position:'right'
-    }
-  }
 
   OpenSenseBoxes.query(function(response) {
     // Assign the response INSIDE the callback
     $scope.data.boxes = response;
   });
 
-  // $scope.productId = $routeParams.state;
-  // $scope.templates =
-  //   [ { name: 'Geräte', url: 'views/devices.html'}
-  //   , { name: 'Sensoren', url: 'template.html'} ];
-  // $scope.template = $scope.templates[0];
-
   $scope.show = function (boxId){
     var elem = "#"+boxId;
     if (boxId != "") {
       $http.get("http://opensensemap.org:8000/boxes/"+boxId+"/sensors").success(function(data,status,headers,config){
-        // console.log(data);
         $scope.selected = data;
-        console.log($scope.selected);
-        // for (var i = $scope.data.boxes.length - 1; i >= 0; i--) {
-        //   console.log($scope.data.boxes[i]._id);
-        //   for (var j = data.length - 1; j >= 0; j--) {
-        //     if ($scope.data.boxes[i]._id == data[j].boxes_id) {};
-        //       $scope.data.boxes[i].sensors = data;
-        //   };
-        //   break;
-        // };
       });  
     };
 
@@ -252,5 +219,103 @@ app.controller('RegisterCtrl', function($scope, $filter, $http) {
       type: null 
     };
     $scope.sensors.push($scope.inserted);
+  };
+});
+
+app.directive('bars', function ($parse) {
+  return {
+     restrict: 'E',
+     replace: true,
+     template: '<div id="chart"></div>',
+     link: function (scope, element, attrs) {
+        var margin = {top: 0, right: 80, bottom: 30, left: 30},
+            width = 450 - margin.left - margin.right,
+            height = 250 - margin.top - margin.bottom;
+
+        var parseDate = d3.time.format("%Y%m%d").parse;
+
+        var x = d3.time.scale()
+            .range([0, width]);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var color = d3.scale.category10();
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+
+        var line = d3.svg.line()
+            .interpolate("basis")
+            .x(function(d) { return x(d.date); })
+            .y(function(d) { return y(d.temperature); });
+
+        var svg = d3.select("#chart").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.tsv("data.tsv", function(error, data) {
+          color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+          data.forEach(function(d) {
+            d.date = parseDate(d.date);
+          });
+
+          var cities = color.domain().map(function(name) {
+            return {
+              name: name,
+              values: data.map(function(d) {
+                return {date: d.date, temperature: +d[name]};
+              })
+            };
+          });
+
+          x.domain(d3.extent(data, function(d) { return d.date; }));
+
+          y.domain([
+            d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
+            d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
+          ]);
+
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Temperature (ºF)");
+
+          var city = svg.selectAll(".city")
+              .data(cities)
+            .enter().append("g")
+              .attr("class", "city");
+
+          city.append("path")
+              .attr("class", "line")
+              .attr("d", function(d) { return line(d.values); })
+              .style("stroke", function(d) { return color(d.name); });
+
+          city.append("text")
+              .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+              .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+              .attr("x", 3)
+              .attr("dy", ".35em")
+              .text(function(d) { return d.name; });
+        });
+     } 
   };
 });

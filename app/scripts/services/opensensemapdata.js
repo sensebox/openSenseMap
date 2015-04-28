532ea9d5d47b883e2cf9b6ae57efe3fa0b0673f5
AlexIsHappy <alexishappy@yandex.ru>
2015-04-28 16:25:41 +0300

    Changes in dates

--- app/scripts/services/opensensemapdata.js
+++ app/scripts/services/opensensemapdata.js
@@ -2,5 +2,5 @@
 
 angular.module('openSenseMapApp')
   .factory('OpenSenseBoxData', function ($resource){
-  	return $resource('http://opensensemap.org:8002/boxes/:boxId/data/:sensorId?from-date=2014-03-25&to-date=2015-04-26',{},{'query': {method: 'GET', isArray: true}});
+  	return $resource('http://opensensemap.org:8002/boxes/:boxId/data/:sensorId?from-date=:date1&to-date=:date2',{},{'query': {method: 'GET', isArray: true}});
   });

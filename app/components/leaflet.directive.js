5ffb0fddb5c21da34b2677fe52b76c25e6bba043
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-07-20 15:47:34 +0200

    add own leaflet directive

---
+++ app/components/leaflet.directive.js
@@ -0,0 +1,158 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('openSenseMapApp')
+    .directive('osemMap', osemMap);
+
+  osemMap.$inject = ['$q', '$rootScope', 'osemMapData'];
+
+  /* @ngInject */
+  function osemMap ($q, $rootScope, osemMapData) {
+    var directive = {
+      templateUrl: 'components/leaflet.directive.html',
+      link: link,
+      restrict: 'EA',
+      transclude: true,
+      controller: function ($scope, $q) {
+        this._leafletMap = $q.defer();
+        this.getMap = function () {
+          return this._leafletMap.promise;
+        };
+
+        this.getLeafletScope = function() {
+          return $scope;
+        };
+      },
+      scope: {
+        markers: '=',
+        center: '=',
+        events: '='
+      }
+    };
+    return directive;
+
+    function link(scope, element, attrs, ctrl) {
+      //TODO: destroy
+      scope.mapId =  attrs.id;
+
+      var map = new L.map(element[0], {
+        minZoom: 2,
+        maxZoom: 18,
+        worldCopyJump: true,
+        scrollWheelZoom: true
+      }).setView([51.04139389812637, 10.2172851562], 6);
+      ctrl._leafletMap.resolve(map);
+
+      map.on('click', onMapClick);
+
+      var markers = L.layerGroup();
+      var oldMarkers = L.layerGroup();
+
+      oldMarkers.on('add', function () {
+        osemMapData.setLayer('oldMarkers', oldMarkers);
+      });
+
+      markers.on('add', function () {
+        osemMapData.setLayer('markers', markers);
+      });
+
+      L.tileLayer('@@OPENSENSEMAP_MAPTILES_URL', {
+        subdomains: 'abc',
+        attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors | Tiles &copy; <a href="http://www.mapbox.com/" target="_blank">Mapbox</a>',
+        detectRetina: true,
+        reuseTiles: true,
+        maxZoom: 18
+      }).addTo(map);
+
+      L.control.scale().addTo(map);
+
+      map.addLayer(markers);
+      map.addLayer(oldMarkers);
+
+      if (angular.isDefined(scope.events) && !angular.equals({}, scope.events)) {
+        if (scope.events.autolocation) {
+          map.locate({setView: true, maxZoom: 16});
+          map.on('locationfound', onLocationFound);
+          map.on('locationerror', onLocationError);
+        }
+      }
+
+      // Resolve the map object to the promises
+      map.whenReady(function() {
+        osemMapData.setMap(attrs.id, map);
+        scope.$watch('markers', function (newVal, oldVal) {
+          if (angular.isDefined(newVal) && !angular.equals({}, newVal)) {
+            markers.clearLayers();
+            oldMarkers.clearLayers();
+            for (var marker in newVal) {
+              var box = newVal[marker];
+              var boxMarker = L.AwesomeMarkers.icon({
+                icon: 'cube',
+                prefix: 'fa',
+                markerColor: box.icon.markerColor
+              });
+              boxMarker.opacity = 0.5;
+              var marker = L.marker([box.lat,box.lng], {icon: boxMarker, options: box, draggable: box.draggable, opacity: box.icon.opacity, zIndexOffset: box.icon.zIndexOffset});
+              if (box.layer === 'oldMarker') {
+                oldMarkers.addLayer(marker);
+              } else {
+                markers.addLayer(marker);
+              }
+              marker.on('click', onMarkerClick);
+              marker.on('mouseover', onMouseOver);
+              marker.on('mouseout', onMouseOut);
+
+              if (box.draggable) {
+                marker.on('dragend', onMarkerDragend);
+              }
+            }
+          }
+        });
+
+        $rootScope.$broadcast('osemMapReady', {});
+      });
+
+      function onLocationFound (e) {
+        var eventName = 'osemMapOnLocationFound.' + scope.mapId;
+        $rootScope.$broadcast(eventName, e);
+        $rootScope.$apply();
+      }
+
+      function onLocationError (e) {
+        var eventName = 'osemMapOnLocationError.' + scope.mapId;
+        $rootScope.$broadcast(eventName, e);
+        $rootScope.$apply();
+      }
+
+      function onMapClick (e) {
+        var eventName = 'osemMapClick.' + scope.mapId;
+        $rootScope.$broadcast(eventName, e);
+        $rootScope.$apply();
+      }
+
+      function onMarkerDragend (e) {
+        var eventName = 'osemMarkerDragend.' + scope.mapId;
+        $rootScope.$broadcast(eventName, e);
+        $rootScope.$apply();
+      }
+
+      function onMouseOver (e) {
+        var eventName = 'osemMarkerMouseOver.' + scope.mapId;
+        $rootScope.$broadcast(eventName, e);
+        $rootScope.$apply();
+      }
+
+      function onMouseOut (e) {
+        var eventName = 'osemMarkerMouseOut.' + scope.mapId;
+        $rootScope.$broadcast(eventName, e);
+        $rootScope.$apply();
+      }
+
+      function onMarkerClick (e) {
+        var eventName = 'osemMarkerClick.' + scope.mapId;
+        $rootScope.$broadcast(eventName, e);
+      }
+    }
+  }
+})();
---
+++ app/components/leaflet.service.js
@@ -0,0 +1,70 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('openSenseMapApp')
+    .factory('osemMapData', osemMapData);
+
+  osemMapData.$inject = ['$q'];
+
+  function osemMapData ($q) {
+    var maps = {};
+    var layers = {};
+    var service = {
+      setMap: setMap,
+      getMap: getMap,
+      getLayer: getLayer,
+      setLayer: setLayer
+    };
+
+    return service;
+
+    ////
+
+    function setMap (mapId, map) {
+      var defer = $q.defer()
+      maps[mapId] = map;
+      defer.resolve(map[mapId]);
+    }
+
+    function getMap (mapId) {
+      var deferred = $q.defer();
+      var found = false;
+      for (var map in maps) {
+        if (mapId === map) {
+          found = true;
+          deferred.resolve(maps[map]);
+        }
+      }
+
+      if (!found) {
+        deferred.reject('Map with mapId ' + mapId + ' not found');
+      }
+
+      return deferred.promise;
+    }
+
+    function setLayer (layerId, layer) {
+      var defer = $q.defer()
+      layers[layerId] = layer;
+      defer.resolve(layers[layerId]);
+    }
+
+    function getLayer (layerId) {
+      var deferred = $q.defer();
+      var found = false;
+      for (var layer in layers) {
+        if (layerId === layer) {
+          found = true;
+          deferred.resolve(layers[layer]);
+        }
+      }
+
+      if (!found ) {
+        deferred.reject('Layer with layerId ' + layerId + ' not found');
+      }
+
+      return deferred.promise;
+    }
+  }
+})();

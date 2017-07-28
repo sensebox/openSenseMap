07d2b038d3fb2822196326b97ab61505188b5dce
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-07-28 10:13:53 +0200

    change leaflet layers and use clustering

--- app/components/leaflet.directive.js
+++ app/components/leaflet.directive.js
@@ -9,6 +9,7 @@
 
   /* @ngInject */
   function osemMap ($q, $rootScope, osemMapData) {
+    var rmax = 30;
     var directive = {
       templateUrl: 'components/leaflet.directive.html',
       link: link,
@@ -42,15 +43,28 @@
 
       map.on('click', onMapClick);
 
-      var markers = L.layerGroup();
-      var oldMarkers = L.layerGroup();
+      var mcg = L.markerClusterGroup({
+            maxClusterRadius: 2*rmax,
+            iconCreateFunction: defineClusterIcon,
+            disableClusteringAtZoom: 17,
+            spiderfyOnMaxZoom: false
+          }),
+          activeMarkerGroup = L.featureGroup.subGroup(mcg),
+          inactiveMarkerGroup = L.featureGroup.subGroup(mcg),
+          oldMarkerGroup = L.featureGroup.subGroup(mcg);
 
-      oldMarkers.on('add', function () {
-        osemMapData.setLayer('oldMarkers', oldMarkers);
+      mcg.addTo(map);
+
+      activeMarkerGroup.on('add', function () {
+        osemMapData.setLayer('activeMarkers', activeMarkerGroup);
       });
 
-      markers.on('add', function () {
-        osemMapData.setLayer('markers', markers);
+      inactiveMarkerGroup.on('add', function () {
+        osemMapData.setLayer('inactiveMarkers', inactiveMarkerGroup);
+      });
+
+      oldMarkerGroup.on('add', function () {
+        osemMapData.setLayer('oldMarkers', oldMarkerGroup);
       });
 
       L.tileLayer('@@OPENSENSEMAP_MAPTILES_URL', {
@@ -63,8 +77,9 @@
 
       L.control.scale().addTo(map);
 
-      map.addLayer(markers);
-      map.addLayer(oldMarkers);
+      activeMarkerGroup.addTo(map); // Adding to map now adds all child layers into the parent group.
+      inactiveMarkerGroup.addTo(map);
+      oldMarkerGroup.addTo(map);
 
       if (angular.isDefined(scope.events) && !angular.equals({}, scope.events)) {
         if (scope.events.autolocation) {
@@ -79,8 +94,9 @@
         osemMapData.setMap(attrs.id, map);
         scope.$watch('markers', function (newVal, oldVal) {
           if (angular.isDefined(newVal) && !angular.equals({}, newVal)) {
-            markers.clearLayers();
-            oldMarkers.clearLayers();
+            activeMarkerGroup.clearLayers();
+            inactiveMarkerGroup.clearLayers();
+            oldMarkerGroup.clearLayers();
             for (var marker in newVal) {
               var box = newVal[marker];
               var boxMarker = L.AwesomeMarkers.icon({
@@ -96,9 +112,11 @@
                 opacity: box.icon.opacity,
                 zIndexOffset: box.icon.zIndexOffset});
               if (box.layer === 'oldMarker') {
-                oldMarkers.addLayer(marker);
+                marker.addTo(oldMarkerGroup);
+              } else if (box.layer === 'inactiveMarer') {
+                marker.addTo(inactiveMarkerGroup);
               } else {
-                markers.addLayer(marker);
+                marker.addTo(activeMarkerGroup);
               }
               marker.on('click', onMarkerClick);
               marker.on('mouseover', onMouseOver);
@@ -153,6 +171,101 @@
       function onMarkerClick (e) {
         var eventName = 'osemMarkerClick.' + scope.mapId;
         $rootScope.$broadcast(eventName, e);
+      }
+
+      function defineClusterIcon (cluster) {
+        var children = cluster.getAllChildMarkers(),
+            n = children.length,
+            strokeWidth = 1,
+            r = rmax-2*strokeWidth-(n<10?12:n<100?8:n<1000?4:0),
+            iconDim = (r+strokeWidth)*2,
+            data = d3.nest()
+              .key(function (d) {
+                return d.options.options['layer'];
+              })
+              .entries(children, d3.map),
+            html = bakeThePie({
+              data: data,
+              valueFunc: function(d){return d.values.length;},
+              strokeWidth: 1,
+              outerRadius: r,
+              innerRadius: r-10,
+              pieClass: 'cluster-pie',
+              pieLabel: n,
+              pieLabelClass: 'marker-cluster-pie-label',
+              pathClassFunc: function(d){return "category-"+d.data.key}
+            }),
+            myIcon = new L.DivIcon({
+              html: html,
+              className: 'marker-cluster',
+              iconSize: new L.Point(iconDim, iconDim)
+            });
+        return myIcon;
+      }
+
+      function bakeThePie (options) {
+        /*data and valueFunc are required*/
+        if (!options.data || !options.valueFunc) {
+          return '';
+        }
+        var data = options.data,
+            valueFunc = options.valueFunc,
+            r = options.outerRadius?options.outerRadius:28, //Default outer radius = 28px
+            rInner = options.innerRadius?options.innerRadius:r-10, //Default inner radius = r-10
+            strokeWidth = options.strokeWidth?options.strokeWidth:1, //Default stroke is 1
+            pathClassFunc = options.pathClassFunc?options.pathClassFunc:function(){return '';}, //Class for each path
+            pathTitleFunc = options.pathTitleFunc?options.pathTitleFunc:function(){return '';}, //Title for each path
+            pieClass = options.pieClass?options.pieClass:'marker-cluster-pie', //Class for the whole pie
+            pieLabel = options.pieLabel?options.pieLabel:d3.sum(data,valueFunc), //Label for the whole pie
+            pieLabelClass = options.pieLabelClass?options.pieLabelClass:'marker-cluster-pie-label',//Class for the pie label
+
+            origo = (r+strokeWidth), //Center coordinate
+            w = origo*2, //width and height of the svg element
+            h = w,
+            donut = d3.pie(),
+            arc = d3.arc().innerRadius(rInner).outerRadius(r);
+
+        //Create an svg element
+        var svg = document.createElementNS(d3.namespaces.svg, 'svg');
+        //Create the pie chart
+        var vis = d3.select(svg)
+            .data([data])
+            .attr('class', pieClass)
+            .attr('width', w)
+            .attr('height', h);
+
+        var arcs = vis.selectAll('g.arc')
+            .data(donut.value(valueFunc))
+            .enter().append('svg:g')
+            .attr('class', 'arc')
+            .attr('transform', 'translate(' + origo + ',' + origo + ')');
+
+        arcs.append('svg:path')
+            .attr('class', pathClassFunc)
+            .attr('stroke-width', strokeWidth)
+            .attr('d', arc);
+
+        vis.append('text')
+            .attr('x',origo)
+            .attr('y',origo)
+            .attr('class', pieLabelClass)
+            .attr('text-anchor', 'middle')
+            //.attr('dominant-baseline', 'central')
+            /*IE doesn't seem to support dominant-baseline, but setting dy to .3em does the trick*/
+            .attr('dy','.3em')
+            .text(pieLabel);
+        //Return the svg-markup rather than the actual element
+        return serializeXmlNode(svg);
+      }
+
+      /*Helper function*/
+      function serializeXmlNode(xmlNode) {
+        if (typeof window.XMLSerializer != "undefined") {
+            return (new window.XMLSerializer()).serializeToString(xmlNode);
+        } else if (typeof xmlNode.xml != "undefined") {
+            return xmlNode.xml;
+        }
+        return "";
       }
     }
   }

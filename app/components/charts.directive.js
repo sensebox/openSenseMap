a9b958f5e50b696c1993c0eb85195da7e26bb66c
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-08-10 16:43:25 +0200

    working d3 line chart with zoom and pan

--- app/components/charts.directive.js
+++ app/components/charts.directive.js
@@ -78,50 +78,56 @@
     function showGraph () {
       console.log('Render Graph');
 
+      var config = vm._chartSVG;
+
       var g = vm._chartSVG.g
           .attr('class', 'focus');
+      var svg = vm._chartSVG.svg;
 
+      //Scales for xAxis and yAxis
       vm.xScale = d3.scaleTime()
-          .rangeRound([0, vm._chartSVG.width]);
+          .rangeRound([0, config.width]);
 
       vm.yScale = d3.scaleLinear()
-          .rangeRound([vm._chartSVG.height, 0]);
+          .rangeRound([config.height, 0]);
 
+      //Setup zoom behaviour
       var zoom = d3.zoom()
-          .scaleExtent([1 / 4, 8])
-          .translateExtent([[-vm._chartSVG.width, -Infinity], [2 * vm._chartSVG.width, Infinity]])
-          .on("zoom", zoomed);
+          .scaleExtent([1 / 2, 8])
+          .translateExtent([[-config.width, -Infinity], [2*config.width, Infinity]])
+          .on('zoom', zoomed);
 
-      var zoomRect = g.append("rect")
-          .attr("width", vm._chartSVG.width)
-          .attr("height", vm._chartSVG.height)
-          .attr("fill", "none")
-          .attr("pointer-events", "all")
-          .call(zoom);
-
-      g.append("clipPath")
-          .attr("id", "clip")
-        .append("rect")
-          .attr("width", vm._chartSVG.width)
-          .attr("height", vm._chartSVG.height);
-
-      vm.line = d3.line()
-        .x(function (d) { return vm.xScale(d.date)})
-        .y(function (d) { return vm.yScale(d.value)});
-
-      //TODO set domain depending on amount of datapoints
+      //Set domains depending on amount of datapoints
       if (vm.chartData.length == 1) {
         vm.xScale.domain([moment(vm.chartData[0][0]).subtract(1,'days'), moment(vm.chartData[0][0]).add(1,'days')]);
         vm.yScale.domain([vm.chartData[0][1]+vm.chartData[0][1], vm.chartData[0][1]-vm.chartData[0][1]]);
       } else {
-        var xExtent = d3.extent(vm.chartData, function (d) { return d.date; });
-        zoom.translateExtent([[vm.xScale(xExtent[0]), -Infinity], [vm.xScale(xExtent[1]), Infinity]]);
-        vm.xScale.domain(d3.extent(vm.chartData, function(d) { return d.date; }));
+        vm.xScale.domain(d3.extent(vm.chartData, function (d) { return d.date; }));
         vm.yScale.domain(d3.extent(vm.chartData, function(d) { return d.value; }));
       }
 
-      vm.xAxis = d3.axisBottom(vm.xScale)
+      //Create Axis and wire up with scale
+      vm.xAxis = d3.axisBottom(vm.xScale);
       vm.yAxis = d3.axisLeft(vm.yScale);
+
+      //Line constructor
+      vm.line = d3.line()
+        .x(function (d) { return vm.xScale(d.date)})
+        .y(function (d) { return vm.yScale(d.value)});
+
+      var zoomRect = g.append('rect')
+          .attr('class', 'zoom-panel')
+          .attr('width', config.width)
+          .attr('height', config.height)
+          .attr('fill', 'none')
+          .attr('pointer-events', 'all')
+          .call(zoom);
+
+      g.append('defs').append('clipPath')
+          .attr('id', 'clipper')
+          .append('rect')
+            .attr('width', config.width)
+            .attr('height', config.height)
 
       // .ticks(d3.utcHour.every(12)).tickFormat(function (d) {
       //   if (moment.utc(d).get('hour') > 0 ) {
@@ -132,9 +138,9 @@
       // });
 
       // 3. Call the x axis in a group tag
-      vm.xGroup = g.append("g")
+      vm.x_Axis = g.append("g")
           .attr("class", "x axis")
-          .attr("transform", "translate(0," + vm._chartSVG.height + ")")
+          .attr("transform", "translate(0," + config.height + ")")
           .call(vm.xAxis); // Create an axis component with d3.axisBottom
 
       // 4. Call the y axis in a group tag
@@ -145,38 +151,39 @@
       // text label for the y axis
       g.append("text")
           .attr("transform", "rotate(-90)")
-          .attr("y", 0 - vm._chartSVG.margin.left)
-          .attr("x",0 - (vm._chartSVG.height / 2))
+          .attr("y", 0 - config.margin.left)
+          .attr("x",0 - (config.height / 2))
           .attr("dy", "1em")
           .style("text-anchor", "middle")
           .text("Value");
 
-      // var temperatur = g.append('g')
-      //     .attr('class', 'temperature')
-      // temperatur.append('path').attr('clip-path', 'url(#clipper)').attr('class', 'line');
-      // temperatur.select('path').transition().duration(500).attr('d', vm.line(vm.chartData));
+      g.append('path')
+          .datum(vm.chartData)
+          .attr('class', 'line')
+          .attr('clip-path','url(#clipper)')
+          .attr('d', vm.line);
 
-      // 9. Append the path, bind the data, and call the line generator
-      vm.linePath = g.append("path")
-          .attr("class", "line") // Assign a class for styling
-          .attr('clipPath', 'url(#clip)')
-          .attr("d", vm.line(vm.chartData)); // 11. Calls the line generator
+      g.append('g')
+          .attr('class', 'dots')
+          .attr('clip-path', 'url(#clipper)')
+          .selectAll('circle')
+          .data(vm.chartData, function (d) {
+            return d.value;
+          })
+          .enter()
+          .append('circle')
+          .attr('class', 'dot')
+          .on('mouseover', mouseover)
+          .on('mouseout', mouseout);
 
-      zoomRect.call(zoom.transform, d3.zoomIdentity);
-      // 12. Appends a circle for each datapoint
-      // focus.append('g')
-      //     .attr('class', 'dots')
-      //     .selectAll("dot")
-      //     .data(vm.chartData)
-      //   .enter().append("circle") // Uses the enter().append() method
-      //     .attr("class", "dot") // Assign a class for styling
-      //     .attr("cx", function(d) { return vm.xScale(d.date) })
-      //     .attr("cy", function(d) { return vm.yScale(d.value) })
-      //     .attr("r", 2.5)
-      //     .on('mouseover', mouseover)
-      //     .on('mouseout', mouseout);
-
-      // vm._chartSVG.svg.call(zoom);
+      g.selectAll('circle')
+          .attr('cy', function (d) {
+            return vm.yScale(d.value);
+          })
+          .attr('cx', function (d) {
+            return vm.xScale(d.date);
+          })
+          .attr('r', 2.5);
 
       if ($scope.chart.chartData) {
         $scope.$watchCollection('chart.chartData', function (newValue, oldValue) {
@@ -188,41 +195,18 @@
     }
 
     function zoomed() {
-      console.log('zoom');
-
-      var xz = d3.event.transform.rescaleX(vm.xScale);
-      vm.xGroup.call(vm.xAxis.scale(xz));
-
-      vm.line = d3.line()
-        .x(function (d) { return xz(d.date)})
-        .y(function (d) { return vm.yScale(d.value)});
-
-      vm.linePath.attr("d", vm.line(vm.chartData));
-      // var t = d3.event.transform, xt = t.rescaleX(vm.x);
-
-      // re-scale y axis during zoom; ref [2]
-      // vm.gX.transition().duration(50).call(vm.xAxis.scale(d3.event.transform.rescaleX(vm.x)));
-
-      // re-draw circles using new y-axis scale; ref [3]
-      // var new_x = d3.event.transform.rescaleX(vm.x);
-      // vm.dots.attr("cy", function(d) { return new_x(d.value); });
-
-      // var t = d3.event.transform, xt = t.rescaleX(vm.x);
-      // vm._chartSVG.g.select(".line").attr("d", vm.line.x(function(d) { return xt(d.date); }));
-      // vm._chartSVG.g.select(".x axis").call(vm.xAxis.scale(xt));
-
-      // vm._chartSVG.svg.selectAll(".line")
-      //   .attr("transform", d3.event.transform);
-      // vm._chartSVG.svg.selectAll(".dot")
-      //   .attr("transform", d3.event.transform);
-      // d3.selectAll('.line').style("stroke-width", 2/d3.event.transform.k);
-      // d3.selectAll('.dot').style("r", 2/d3.event.transform.k);
-      // vm.gX.call(vm.xAxis.scale(d3.event.transform.rescaleX(vm.x)));
-      // vm.gY.call(vm.yAxis.scale(d3.event.transform.rescaleY(vm.y)));
-
-      // var xz = d3.event.transform.rescaleX(x);
-      // xGroup.call(xAxis.scale(xz));
-      // areaPath.attr("d", area.x(function(d) { return xz(d.date); }));
+      console.log('zoomed');
+      var transform = d3.event.transform;
+      var xNewScale = transform.rescaleX(vm.xScale);
+      vm.xAxis.scale(xNewScale);
+      var g = vm._chartSVG.g;
+      g.select('.x.axis').call(vm.xAxis);
+      g.selectAll('circle').attr('cx', function (d) {
+        return transform.applyX(vm.xScale(d.date));
+      });
+      g.select('.line').attr('d', vm.line.x(function (d) {
+        return xNewScale(d.date);
+      }));
     }
 
     function mouseover (d) {

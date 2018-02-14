5ca8d1219025155a66b33f1927745676f28156a9
Matthias Pfeil <matthias.pfeil@wwu.de>
2018-02-14 17:45:10 +0100

    refactor(services): intercept also translation files
    Append timestamp to translation files to prevent cache problems

--- app/scripts/services/templateInterceptor.js
+++ app/scripts/services/templateInterceptor.js
@@ -10,7 +10,7 @@
     return {
       'request': function (config) {
 
-        if (config.url.indexOf('views') !== -1 || config.url.indexOf('components') !== -1) {
+        if (config.url.indexOf('views') !== -1 || config.url.indexOf('components') !== -1 || config.url.indexOf('translations') !== -1) {
           var d = new Date();
           var ms = d.getTime();
           config.url = config.url + '?t=' + ms;

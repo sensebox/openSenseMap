82279bdf54de434c335818d9aee59cb14c454e8b
felixerdy <felix.erdmann@uni-muenster.de>
2020-07-10 13:23:47 +0200

    remove comments containing old code

--- app/scripts/controllers/sidebar.download.js
+++ app/scripts/controllers/sidebar.download.js
@@ -156,7 +156,6 @@
         }
         var query = $httpParamSerializer(params);
 
-        // return encodeURI(app.API_URL + '/' + endpoint + '?' + query);
         return app.API_URL + '/' + endpoint + '?' + query;
       }
 

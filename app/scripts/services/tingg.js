f218b373374cd0fbf459bedddf18261bcc3fc278
Thiemann96 <e_thie10@uni-muenster.de>
2020-12-01 12:32:58 +0100

    added login and refresh token methods

--- app/scripts/services/tingg.js
+++ app/scripts/services/tingg.js
@@ -14,13 +14,15 @@
      * 
      */
     angular
-        .module('app.services', [])
+        .module('app.services')
         .factory('TinggService', TinggService);
 
     TinggService.$inject = ['$http', '$window', 'app'];
 
     function TinggService($http, $window, app) {
         var service = {
+            login:login,
+            refreshToken:refreshToken,
             verifyModem: verifyModem,
             createThingType: createThingType,
             createThing: createThing,
@@ -31,6 +33,31 @@
 
         function failed(error) {
             return $q.reject(error.data);
+        }
+
+        /**
+         * logs into tingg developer account
+         * @param {"email":"email","password":"password"} data 
+         */
+        function login(data){
+            console.log("logging in",data);
+            return $http.post(app.TINGG_URL + '/auth/login',data)
+                .then(function(response){
+                    console.log("login success",response)
+                })
+                .catch(failed)
+        }
+        /**
+         * gets new token based on old one
+         * @param {"token":token} data 
+         */
+        function refreshToken(data){
+            console.log("refresh token");
+            return $http.post(app.TINGG_URL + '/auth/token-refresh',data)
+            .then(function(response){
+                console.log("refresh token",response)
+            })
+            .catch(failed)
         }
         /* calls   GET https://api.tingg.io/v1/modems/:imsi/verify?code=:code to verify imsi and secret code
           input: imsi and secret code from register ui

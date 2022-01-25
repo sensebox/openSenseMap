98d9aef73c2ca765d1ba2a60c5d8c067311cc242
Matthias Pfeil <matthias.pfeil@wwu.de>
2022-01-25 15:39:46 +0100

    Add announcement directive with directus connection

---
+++ app/components/announcement.directive.js
@@ -0,0 +1,79 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('openSenseMapApp')
+    .directive('osemAnnouncement', osemAnnouncement);
+
+  osemAnnouncement.$inject = [];
+
+  function osemAnnouncement () {
+    var directive = {
+      templateUrl: 'components/announcement.directive.html',
+      link: link,
+      // eslint-disable-next-line angular/directive-restrict
+      restrict: 'EA',
+      transclude: true,
+      controller: AnnouncementController,
+      controllerAs: 'announcement',
+      bindToController: true, // because the scope is isolated
+      scope: {},
+    };
+
+    return directive;
+
+    ////
+
+    function link (scope, element, attrs) {}
+  }
+
+  AnnouncementController.$inject = ['$scope', 'DirectusService'];
+
+  function AnnouncementController ($scope, DirectusService) {
+    var vm = this;
+    var variantPrefix = 'alert-';
+
+    vm.$onInit = onInit;
+    vm.$onDestroy = onDestroy;
+
+    vm.show = true;
+    vm.variant = 'alert-warning';
+    vm.markdown = '';
+
+    vm.close = close;
+
+    ////
+
+    function onInit () {
+      activate();
+    }
+
+    function onDestroy () {
+      // $timeout.cancel(vm.prom);
+      // resetTimer();
+    }
+
+    function activate () {
+      // fetch CMS endpoint
+      return DirectusService.getAnnouncement()
+        .then(function (response) {
+          vm.variant = variantPrefix + response.data.variant;
+          vm.markdown = response.data.text;
+        })
+        .catch(function () {
+          // no need to catch errors
+          // instead hide announcment
+          vm.show = false;
+        });
+    }
+
+    function close () {
+      vm.show = false;
+
+      $scope.$emit('osemAnnouncementClosed', {});
+    }
+
+    ////
+
+  }
+})();

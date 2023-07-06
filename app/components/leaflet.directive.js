e8caaf98e13217158e60a7c844f56c1c68173ffe
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-07-06 12:12:43 +0200

    refactor(map): switch back to Mapbox
    Switch back to Mapbox and use mapbox-gl-js

--- app/components/leaflet.directive.js
+++ app/components/leaflet.directive.js
@@ -96,14 +96,16 @@
       });
 
       const key = '@@OPENSENSEMAP_ACCESS_TOKEN';
-      const styleUrl = '@@OPENSENSEMAP_STYLE_URL'
-      const gl = L.maplibreGL({
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        style: styleUrl + "?key=" + key
-      }).addTo(map);
-      var maplibreMap = gl.getMaplibreMap();
+      const styleUrl = '@@OPENSENSEMAP_STYLE_URL';
 
-      maplibreMap.on('load', layerLoaded);
+      var gl = L.mapboxGL({
+        attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+        accessToken: '@@OPENSENSEMAP_ACCESS_TOKEN',
+        style: '@@OPENSENSEMAP_STYLE_URL'
+      }).addTo(map);
+      var mapboxMap = gl.getMapboxMap();
+
+      mapboxMap.on('load', layerLoaded);
 
       L.control.scale().addTo(map);
 
@@ -153,7 +155,7 @@
       function layerLoaded () {
         $rootScope.$broadcast('layerloaded', {});
         $rootScope.$apply();
-        maplibreMap.off('load', layerLoaded);
+        mapboxMap.off('load', layerLoaded);
         for (var layerName in mapLayers) {
           mapLayers[layerName].on('add', function () {
             osemMapData.setLayer(layerName, mapLayers[layerName]);
